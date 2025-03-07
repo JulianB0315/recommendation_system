ddf581d38204f44dfea191dd0a61ac00fdeb207c
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-03-07 15:02:35 -0500

    Update ejemplos_skearn.py

--- scripts/ejemplos_skearn.py
+++ scripts/ejemplos_skearn.py
@@ -1,8 +1,9 @@
 from sklearn.cluster import KMeans
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Cargar datos
-datos = pd.read_csv('../data/ventas.csv')
+datos = pd.read_csv('data/ventas.csv')
 
 # Datos de ejemplo
 X = datos[['cantidad', 'precio']]
@@ -13,4 +14,15 @@ kmeans.fit(X)
 
 # Predicciones
 clusters = kmeans.predict(X)
-print(clusters)
+
+# Agregar los clusters al DataFrame
+datos['cluster'] = clusters
+
+# Visualización de los clusters
+plt.figure(figsize=(10, 6))
+plt.scatter(datos['cantidad'], datos['precio'], c=datos['cluster'], cmap='viridis')
+plt.xlabel('Cantidad')
+plt.ylabel('Precio')
+plt.title('Clusters de Ventas')
+plt.colorbar(label='Cluster')
+plt.show()
